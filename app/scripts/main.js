808bae72f92dd854d77609d10e1f7150f38bb0bf
phil <uniphil@gmail.com>
2014-06-06 16:36:16 -0400

    Use underscore for templating.
    The text loader plugin (installs with `$ bower install` on the project) is used to pull in the html string for the template.
    
    There is not much more to say here, it works!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,14 @@
 require.config({
   baseUrl: "scripts",
   paths: {
+    // external dependencies
     "jquery": "vendor/jquery/dist/jquery",
     "underscore": "vendor/underscore-amd/underscore",
-    "backbone": "vendor/backbone-amd/backbone"
+    "backbone": "vendor/backbone-amd/backbone",
+    // plugins
+    "text": "vendor/requirejs-text/text",
+    // local stuff
+    "templates": "../templates"
   }
 });
 
--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -1,10 +1,22 @@
 define(function (require) {
-  var Backbone = require('backbone');
+  var _           = require('underscore'),
+      Backbone    = require('backbone'),
+      BoxTemplate = require('text!templates/box.html');
 
   var App = Backbone.View.extend({
+
     initialize: function() {
-      console.log('initialized yo');
+      this.render();
+    },
+
+    el: '#thing',
+
+    template: _.template(BoxTemplate),
+
+    render: function() {
+      this.$el.html(this.template({name: prompt('your name please, ')}));
     }
+
   });
 
   return App;
